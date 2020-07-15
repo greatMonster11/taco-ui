936e7c4666680e6d3ca6cf1539930f418ad84180
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-07-15 09:53:41 +0700

    add specials component

---
+++ src/app/specials/specials.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from '@angular/core'
+
+@Component({
+    selector: 'taco-specials',
+    template: 'specials.component.html'
+})
+
+export class SpecialsComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
