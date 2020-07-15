34271ae64dfaa5ac3ea595832b7a8600958e220a
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-07-15 09:13:14 +0700

    add locations components

---
+++ src/app/locations/locations.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core'
+
+@Component({
+    selector: 'locations-tacocloud',
+    template: 'locations.component.html',
+    styleUrls: ['./locations.component.css']
+})
+
+export class LocationsComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
