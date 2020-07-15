55ec63f34dd814fc7cb97ee54b7035f46c8dc903
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-07-15 09:47:59 +0700

    add recent component

---
+++ src/app/recents/RecentTacosService.ts
@@ -0,0 +1,11 @@
+import {Injectable} from '@angular/core'
+import {ApiService} from '../api/ApiService'
+
+@Injectable()
+export class RecentTacosService {
+    constructor(private apiService: ApiService) {}
+
+    getRecentTacos() {
+        return this.apiService.get('design/recent')
+    }
+}
---
+++ src/app/recents/WrapPipe.ts
@@ -0,0 +1,20 @@
+import {Pipe, PipeTransform} from '@angular/core'
+
+@Pipe({
+    name: 'wraps'
+})
+
+export class WrapPipe implements PipeTransform {
+    transform(ingredients: any, ...args: any): any {
+        const wraps = [];
+        for (const ingredient of ingredients) {
+            if (ingredient.type === 'WRAP') {
+                const wrap: any = {}
+                wrap.id = ingredient.id
+                wrap.name = 'a' + ingredient.name
+                wraps.push(wrap)
+            }
+        }
+        return wraps
+    }
+}
