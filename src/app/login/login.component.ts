173848bc09e0fe5c79bda399accd6bb818a0aa5d
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-07-15 09:26:11 +0700

    add login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core'
+
+@Component({
+    selector: 'login-tacocloud',
+    template: 'login.component.html',
+    styleUrls: ['login.component.css']
+})
+
+export class LoinComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
